e7538097e14439d702b1d3c4ca582f56a311df1d
Otio <alanood28albattashi@gmail.com>
2026-09-02 00:48:56 +0400

    Delete Cyber-AI directory

--- Cyber-AI/scripts/collect_data.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# Collect and clean data sources
-
-cd "$(dirname "$0")/.."
-python -m crawler.crawler
--- Cyber-AI/scripts/train.sh
+++
@@ -1,5 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# Start training / fine-tuning
-
-cd "$(dirname "$0")/.."
-python -m model.training.train
